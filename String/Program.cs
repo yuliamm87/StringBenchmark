92c7e04ef149579a59a4ca7a0af782861de8f305
yuliiaserhiienko <imk@simcorp.com>
2021-11-19 14:54:00 +0200

    string benchmark

---
+++ String/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+namespace String
+{
+    class Program
+    {
+        static void Main(string[] args) {
+
+            string str = "hello";
+            str += "IMK1";
+            str += "IMK2";
+            str += "IMK3";
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ StringBenchmark/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Mathematics;
+using BenchmarkDotNet.Running;
+//https://www.infoworld.com/article/3573782/how-to-benchmark-csharp-code-using-benchmarkdotnet.html
+
+namespace StringBenchmark
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            BenchmarkRunner.Run<BenchmarkStringConcatenation>();
+            Console.ReadKey();
+        }
+    }
+    [RankColumn]
+    [MemoryDiagnoser]
+    public class BenchmarkStringConcatenation
+    {
+        [Benchmark]
+        public void StringCatenation() {
+            string myString = string.Empty;
+            for (int i = 0; i < 1000; i++) {
+                myString += "Hello";
+            }
+        }
+        [Benchmark]
+        public void StringBuilder() {
+            var stringBuilder = new StringBuilder();
+            for (int i = 0; i < 1000; i++) {
+                stringBuilder.Append("Hello");
+            }
+
+            var targetString = stringBuilder.ToString();
+        }
+    }
+}
